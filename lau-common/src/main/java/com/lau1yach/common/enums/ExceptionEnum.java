4423064dac3149f12a56dbeacd8bb689713a9ef4
LauWingHung <lyhplayer@me.com>
2018-11-30 16:33:05 +0800

    update

--- lau-common/src/main/java/com/lau1yach/common/enums/ExceptionEnum.java
+++ lau-common/src/main/java/com/lau1yach/common/enums/ExceptionEnum.java
@@ -14,12 +14,14 @@ import lombok.*;
 @NoArgsConstructor
 public enum ExceptionEnum {
     /**
-     * price cannot be null
+     *
      */
     BRAND_NOT_FOUND(400,"品牌没查到!"),
     CATEGORY_NOT_FOUND(404,"商品分类没查到!"),
     BRAND_SAVE_ERROR(500,"品牌新增失败!"),
     CATEGORY_BRAND_SAVE_ERROR(500,"品牌分类新增失败!"),
+    UPLOAD_FILE_ERROR(500,"文件上传失败!"),
+    INVALID_FILE_TYPE(500,"无效的文件类型!"),
     ;
     private int code;
     private String msg;
--- lau-upload/src/main/java/com/lau1yach/upload/service/UploadService.java
+++ lau-upload/src/main/java/com/lau1yach/upload/service/UploadService.java
@@ -1,8 +1,16 @@
 package com.lau1yach.upload.service;
 
+import com.lau1yach.common.enums.ExceptionEnum;
+import com.lau1yach.common.exception.LauException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author lyhpl
@@ -12,16 +20,36 @@ import org.springframework.web.multipart.MultipartFile;
  * @date 2018/11/26  11:10
  */
 @Service
+@Slf4j
 public class UploadService {
 
+    private static final List<String> ALLOW_TYPE = Arrays.asList("image/jpeg","image/png","image/bmp");
+
     public String uploadImage(MultipartFile file) {
+        try {
+            //校验文件
+            String contentType = file.getContentType();
+            if (!ALLOW_TYPE.contains(contentType)){
+                throw new LauException(ExceptionEnum.INVALID_FILE_TYPE);
+            }
 
-        //目标路径
+            //校验文件内容
+            BufferedImage image = ImageIO.read(file.getInputStream());
+            if (image == null){
+                throw new LauException(ExceptionEnum.INVALID_FILE_TYPE);
+            }
 
-        //保存文件到本地
+            //目标路径
+            File dest = new File("D:/JakartaEE/lau1yach/upload/",file.getOriginalFilename());
+            //保存文件到本地
+            file.transferTo(dest);
 
-        //返回路径
-
-        return null;
+            //返回路径
+            return "http://image.lau1yach.com"+file.getOriginalFilename();
+        } catch (IOException e) {
+            //上传失败
+            log.error("上传失败!",e);
+            throw new LauException(ExceptionEnum.UPLOAD_FILE_ERROR);
+        }
     }
 }
